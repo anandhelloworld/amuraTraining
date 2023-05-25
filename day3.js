87964b39605782fdfde3cee4e1dd9152fde47532
Anand Mohan Babu <106240435+anandhelloworld@users.noreply.github.com>
2023-05-25 11:13:22 +0530

    Update promises

--- day3.js
+++ day3.js
@@ -32,5 +32,101 @@ console.log("b:",b)
 
 
 
+//Promises
+function promise (resolve,reject){
+    
+    let a=1==1
+    
+     if(a){
+        resolve()
+    }else{
+        reject()
+    }
+}
+
+function resolve(){
+   console.log("resolved")
+}
+
+function reject(){
+     console.log("reject")
+}
+
+promise(resolve,reject)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let cart=["shoes","watch"]
+
+let cartCheckout = new Promise((res,rej)=>{
+    if(true){
+        res("1")
+    }else{
+        rej("0")
+    }
+})
+cartCheckout.then((data)=>{
+    console.log(data)
+    return "3"
+})
+.then((dat)=>{
+    console.log(dat)
+})
+
+
+.catch((error)=>{
+    console.log(error)
+}).finally(()=>{
+    console.log("finaaly do it")
+})
+
+
+
+let cart=["shoes","watch"]
+
+let cartCheckout = new Promise((res,rej)=>{
+    setTimeout(()=>{
+        if(true){
+        res("1")
+    }else{
+        rej("0")
+    }
+    },5000)
+    
+})
+console.log(cartCheckout)
+cartCheckout.then((data)=>{
+    console.log(data)
+    return "3"
+})
+.then((dat)=>{
+    console.log(dat)
+})catch().then().then()
+.catch((error)=>{
+    console.log(error)
+}).finally(()=>{
+    console.log("finaaly do it")
+})
+
+console.log(cartCheckout)
+
+ let takingInfo = new promise()
+ 
+
 
 
